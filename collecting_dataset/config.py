ce0e58f89b17ac90f367a139f5694fc95edcab98
Mikhail Naumov <mr.naumov.m@mail.ru>
2022-10-19 17:48:01 +0300

    updates

--- collecting_dataset/config.py
+++ collecting_dataset/config.py
@@ -1,5 +1,13 @@
 DATASET_PATH = '/home/mikhail/Documents/GitHub/mushroom_classify/mushroom_dataset'
 
-RU_CLASSES_URL = 'https://ru.wikipedia.org/w/index.php?title=%D0%9A%D0%B0%D1%82%D0%B5%D0%B3%D0%BE%D1%80%D0%B8%D1%8F:%D0%93%D1%80%D0%B8%D0%B1%D1%8B_%D0%BF%D0%BE_%D0%B0%D0%BB%D1%84%D0%B0%D0%B2%D0%B8%D1%82%D1%83'
+url_edible = 'https://ru.wikipedia.org/wiki/%D0%9A%D0%B0%D1%82%D0%B5%D0%B3%D0%BE%D1%80%D0%B8%D1%8F:%D0%A1%D1%8A%D0%B5%D0%B4%D0%BE%D0%B1%D0%BD%D1%8B%D0%B5_%D0%B3%D1%80%D0%B8%D0%B1%D1%8B'
+url_inedible = 'https://ru.wikipedia.org/wiki/%D0%9A%D0%B0%D1%82%D0%B5%D0%B3%D0%BE%D1%80%D0%B8%D1%8F:%D0%9D%D0%B5%D1%81%D1%8A%D0%B5%D0%B4%D0%BE%D0%B1%D0%BD%D1%8B%D0%B5_%D0%B3%D1%80%D0%B8%D0%B1%D1%8B'
+RU_CLASSES_URLS = [url_edible, url_inedible]
+
+HEADERS = {
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 '
+                  'Safari/537.36 '
+}
 
 DRIVER_PATH = "/usr/bin/chromedriver"  # chromedriver location
--- collecting_dataset/mushroom_list.py
+++ collecting_dataset/mushroom_list.py
@@ -1,46 +1,72 @@
 import requests
 from bs4 import BeautifulSoup
 import os
-from config import RU_CLASSES_URL
+from config import RU_CLASSES_URLS, HEADERS
 
-FILE = 'Категория_Грибы по алфавиту — Википедия.html'
-URL = RU_CLASSES_URL
+FILES = ['edible.html', 'inedible.html']
+URLS = RU_CLASSES_URLS
 
 
-def load_url_contents_cached(url, local_file):
-    # if exist read local file
-    if os.path.exists(local_file):
-        with open(local_file, 'r', encoding="utf-8") as f:
-            contents = f.read()
-    else:
-        # if not exist download
-        contents = requests.get(url).text
+def load_url_contents_cached(urls, local_files):
+    contents = []
+    for url, local_file, i in zip(urls, local_files, range(len(local_files))):
+        # if exist read local file
+        if os.path.exists(local_file):
+            with open(local_file, 'r', encoding="utf-8") as f:
+                contents.append(f.read())
+        else:
+            # if not exist download
+            contents.append(requests.get(url).text)
 
-        # save in file
-        with open(local_file, 'w', encoding="utf-8") as f:
-            f.write(contents)
+            # save in file
+            with open(local_file, 'w', encoding="utf-8") as f:
+                f.write(contents[i])
     return contents
 
 
 def get_title(contents):
-    soup = BeautifulSoup(contents, 'lxml')
-    titles = soup.find(id="mw-pages").find_all('li')
-    ru_titles = []
+    list_classes = []
+    for content in contents:
+        classes = []
 
-    for i in range(len(titles)):
-        ru_titles.append(titles[i].text)
+        soup = BeautifulSoup(content, 'lxml')
+        ru_classes = soup.find(id="mw-pages").find_all('li')
+        next_link = 'https://ru.wikipedia.org' + soup.find('a', text='Следующая страница').get('href')
 
-    return ru_titles
+        for item in ru_classes:
+            classes.append(item.text)
+
+        soup = BeautifulSoup(requests.get(next_link, headers=HEADERS).text, 'lxml')
+        ru_classes1 = soup.find(id="mw-pages").find_all('li')
+
+        for item in ru_classes1:
+            classes.append(item.text)
+
+        list_classes.append(classes)
+
+    list_classes[0] = list_classes[0][2:]
+
+    return list_classes
 
 
 def get_wiki_species():
-    content = load_url_contents_cached(URL, FILE)
-    classes = get_title(content)
-    classes = classes
-    print('number of classes:', len(classes))
+    content = load_url_contents_cached(URLS, FILES)
+    list_classes = get_title(content)
+    # classes = classes
+    print('number of edible classes:', len(list_classes[0]))
+    print('number of inedible classes:', len(list_classes[1]))
+    print('number of total classes:', len([*list_classes[0], *list_classes[1]]))
+
+    with open('edible.txt', 'w', encoding="utf-8") as f:
+        for mushroom in list_classes[0]:
+            f.write(mushroom + '\n')
+
+    with open('inedible.txt', 'w', encoding="utf-8") as f:
+        for mushroom in list_classes[1]:
+            f.write(mushroom + '\n')
 
     with open('classes.txt', 'w', encoding="utf-8") as f:
-        for mushroom in classes:
+        for mushroom in [*list_classes[0], *list_classes[1]]:
             f.write(mushroom + '\n')
 
 
